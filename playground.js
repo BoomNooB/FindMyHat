469a5a7feaf92de064928c7a1de8b6a41920fc1a
BoomThinkpad <boom@live.se>
2023-03-30 17:59:23 +0700

    Done with Random start Spawn

--- playground.js
+++ playground.js
@@ -1,13 +1,14 @@
-const prompt = require('prompt-sync')();
+// const prompt = require('prompt-sync')();
 
-// const name = prompt('What is your name?');
-// console.log(`Hey there ${name}`);
+// // const name = prompt('What is your name?');
+// // console.log(`Hey there ${name}`);
 
 
-// let myGrid = [...Array(6)].map(e => Array(6).fill('░'));
-// console.log(myGrid);
+// // let myGrid = [...Array(6)].map(e => Array(6).fill('░'));
+// // console.log(myGrid);
 
-let x  = parseInt(prompt(`Please enter how long of column axis:`))
-let y  = parseInt(prompt(`Please enter how long of row axis:`))
-let hole  = parseInt(prompt(`Please enter how much of hole you wish to in percentage(1-100):`))
-console.log(`${x} ${y} ${hole}`)
+// let x  = parseInt(prompt(`Please enter how long of column axis:`))
+// let y  = parseInt(prompt(`Please enter how long of row axis:`))
+// let hole  = parseInt(prompt(`Please enter how much of hole you wish to in percentage(1-100):`))
+// console.log(`${x} ${y} ${hole}`)
+console.log('90'.toLowerCase());
